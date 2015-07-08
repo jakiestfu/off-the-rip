31db7c892ecac529908c9cf1863065d9df191ba4
Jacob Kelley <jakie8@gmail.com>
2015-07-07 19:12:00 -0700

    Removing global phantomjs dependency

--- scripts/pre-install.sh
+++ scripts/pre-install.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
-  sudo npm install -g phantomjs
   sudo apt-get --assume-yes install eyeD3 rtmpdump
 elif [[ "$OSTYPE" == "darwin"* ]]; then
-  brew install eyeD3 phantomjs rtmpdump
+  brew install eyeD3 rtmpdump
 else
   echo "UNKNOWN OS"
 fi
