3866eadcc7d891636027822edbed365b743c7b4d
Jacob Kelley <jakie8@gmail.com>
2015-07-07 18:04:51 -0700

    Better logging, fixed a linux dependency

--- scripts/pre-install.sh
+++ scripts/pre-install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
-  npm install -h phantomjs
+  npm install -g phantomjs
   sudo apt-get --assume-yes install eyeD3 rtmpdump
 elif [[ "$OSTYPE" == "darwin"* ]]; then
   brew install eyeD3 phantomjs rtmpdump
